58a30335200aadfa6dff800267d02c6a6bb07927
zihong <mzUafXdlDkR%>
2019-06-11 15:00:34 +0800

    完善基于枚举实现單例

--- SingletonMode/src/main/java/com/dashuai/learning/singletonmode/simple/EnumSingleton.java
+++ SingletonMode/src/main/java/com/dashuai/learning/singletonmode/simple/EnumSingleton.java
@@ -8,16 +8,44 @@ package com.dashuai.learning.singletonmode.simple;
  *
  * @author Liaozihong
  */
-public enum EnumSingleton {
-    /**
-     * Instance enum singleton.
-     */
-    INSTANCE;
+public class EnumSingleton {
 
     /**
-     * Whatever method.
+     * My enum singleton
+     * <p/>
+     * Created in 2019.06.11
+     * <p/>
+     *
+     * @author Liaozihong
      */
-    public void whateverMethod() {
+    public enum MyEnumSingleton {
+        /**
+         * Enum factory my enum singleton.
+         */
+        enumFactory;
+        private EnumSingleton instance;
 
+        private MyEnumSingleton() {
+            instance = new EnumSingleton();
+        }
+
+        /**
+         * Gets instance.
+         *
+         * @return the instance
+         */
+        public EnumSingleton getInstance() {
+            return instance;
+        }
+
+    }
+
+    /**
+     * Gets instances.
+     *
+     * @return the instances
+     */
+    public static EnumSingleton getInstances() {
+        return MyEnumSingleton.enumFactory.getInstance();
     }
 }
